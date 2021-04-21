38203f1838a550be2fa6742e6ac2418cf973c296
dock6699 <82054005+dock6699@users.noreply.github.com>
2021-04-21 19:04:07 +0300

    add

---
+++ HealthBar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private float _damage;
+    [SerializeField] private float _heal;
+    [SerializeField] private float _maxHealth;
+
+    public event UnityAction OnHealthChanged;
+
+    private float _health;
+    public float MaxHealth => _maxHealth;
+    public float Health => _health;
+
+    private void Awake()
+    {
+        _health = _maxHealth;
+    }
+    public void TakeDamage()
+    {
+        if (_health >= _damage)
+        {
+            _health = ChangeHealth(-_damage);
+            OnHealthChanged?.Invoke();
+        }
+        else
+        {
+            _health = 0;
+            OnHealthChanged?.Invoke();
+        }
+    }
+
+    public void Heal()
+    {
+        if (_health < _maxHealth)
+        {
+            _health = ChangeHealth(_heal);
+            OnHealthChanged?.Invoke();
+        }
+        else
+        {
+            _health = _maxHealth;
+            OnHealthChanged?.Invoke();
+        }
+    }
+
+    private float ChangeHealth(float value)
+    {
+        float newHealthValue;
+        newHealthValue = _health + value;
+        return newHealthValue;
+    }
+
+
+}
